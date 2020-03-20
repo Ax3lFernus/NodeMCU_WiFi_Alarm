278c6612408fb246bb350828686b92156a0a08a2
Alessandro Annese <a.annese99@gmail.com>
2020-03-20 15:01:28 +0100

    Updated firmware to v1.0

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -19,10 +19,10 @@
 #include <ESP8266WiFi.h>
 
 // Network Credentials
-const char* ssid     = "FASTWEB-RG6JPF";
-const char* password = "WBNB7DKIDC";
+const char* ssid     = ""; //Network SSID
+const char* password = ""; //Network PASSWORD
 
-// Set web server port number to 80
+// Set WebServer Port to 80
 WiFiServer server(80);
 
 // Variable to store the HTTP request
@@ -35,18 +35,19 @@ unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
-// Pin setup
-const int doorPin = D1, boardLed = D0, h24Pin = D2, activeAlarmLed = D3;
-
-bool alarmActive = false, inAlarm = false;
-
 // Alarm time counting
 // Current time
 unsigned long alarmCurrentTime = millis();
 // Previous time
 unsigned long alarmPreviousTime = 0; 
-// Define timeout time in milliseconds (example: 60000ms = 60s)
+// Define siren sound time in case of alarm (in milliseconds, example: 60000ms = 60s)
 const long alarmTimeout = 10000;
+
+// Board Pin setup
+const int doorPin = D1, boardLed = D0, h24Pin = D2, activeAlarmLed = D3;
+
+// Define flags
+bool alarmActive = false, inAlarm = false;
 
 void setup() {
   pinMode(doorPin,INPUT_PULLUP);
@@ -57,34 +58,27 @@ void setup() {
   digitalWrite(boardLed, HIGH);
   
   Serial.begin(115200);
-  
-  // Connect to Wi-Fi network with SSID and password
-  //Serial.print("Connecting to ");
-  //Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    //Serial.print(".");
   }
-  // Print local IP address and start web server
-  //Serial.println("");
-  Serial.println("WiFi connected.");
-  //Serial.println("IP address: ");
-  //Serial.println(WiFi.localIP());
+  Serial.println("Connected. Alarm ready.");
   server.begin();
 }
 
 void loop(){
   WiFiClient client = server.available();   // Listen for incoming clients
-  
+
+  // H24 Line protection
   if(digitalRead(h24Pin) == HIGH){
     inAlarm = true;
   }
-  
+
+  // Alarm active control
   if(alarmActive){
     digitalWrite(activeAlarmLed, HIGH);
     
-    if(digitalRead(doorPin) == HIGH){
+    if(digitalRead(doorPin) == HIGH){ //If door is opened, start the siren alarm
       inAlarm = true;
     }
   }else{
@@ -115,7 +109,6 @@ void loop(){
       currentTime = millis();         
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
-        //Serial.write(c);                    // print it out the serial monitor
         header += c;
         if (c == '\n') {                    // if the byte is a newline character
           // if the current line is blank, you got two newline characters in a row.
@@ -134,12 +127,10 @@ void loop(){
             // Verify if are present commands in HTTP header
             if (header.indexOf("GET /1/on") >= 0) {
               alarmActive = true; //Set alarm flag true (Alarm is active)
-              //digitalWrite(boardLed, LOW);
               client.println("{\"result\":true,\"msg\":\"Alarm activated\"}");
             }else if(header.indexOf("GET /1/off") >= 0){
               alarmActive = false; //Set alarm flag false (Alarm is deactivated)
-              inAlarm = false;
-              //digitalWrite(boardLed, HIGH);
+              inAlarm = false; //Set the alarm status to false (Siren doesn't sound)
               client.println("{\"result\":true,\"msg\":\"Alarm deactivated\"}");
             }else if(header.indexOf("GET /status") >=0){
               client.println(digitalRead(doorPin) == HIGH ? "{\"door\":\"open\"" : "{\"door\":\"closed\"");
@@ -166,7 +157,5 @@ void loop(){
     header = "";
     // Close the connection
     client.stop();
-    //Serial.println("Client disconnected.");
-    //Serial.println("");
   }
 }
