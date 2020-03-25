e26143bb29b92962315cdcea7781e79723d75b5e
Ax3lFernus <a.annese99@gmail.com>
2020-03-25 22:09:07 +0100

    Update firmware.ino

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -12,7 +12,7 @@
  * Developed by Alessandro Annese 
  * GitHub: Ax3lFernus
  * E-Mail: a.annese99@gmail.com
- * Version v2.1.5 23-03-2020
+ * Version v2.1.6 25-03-2020
  */
 
 // Load Wi-Fi library
