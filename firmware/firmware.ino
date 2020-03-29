2a9c35294e062374a8de476880486fa2d6631245
Ax3lFernus <a.annese99@gmail.com>
2020-03-29 19:52:48 +0200

    Update firmware.ino
    - Switch server to HTTPS
    - Implemented API KEY
    - Changed WebServer library and code

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -12,12 +12,16 @@
  * Developed by Alessandro Annese 
  * GitHub: Ax3lFernus
  * E-Mail: a.annese99@gmail.com
- * Version v2.2 29-03-2020
+ * Version v2.3 29-03-2020
  */
 
 // Load Wi-Fi library
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WebServerSecure.h>
 #include <MFRC522.h>
+
 #define SS_PIN D4
 #define RST_PIN D3
 
@@ -29,8 +33,11 @@ const char *password = ""; //Network PASSWORD
 const char *UID = "";      //UID Card Code
 const char *API_KEY = "";  //API KEY
 
-// Set WebServer Port to 80
-WiFiServer server(80);
+const char *dname = "allarme.local"; //Local address
+
+// Set WebServer (HTTP & HTTPS)
+BearSSL::ESP8266WebServerSecure server(443);
+ESP8266WebServer serverHTTP(80);
 
 // Variable to store the HTTP request
 String header;
@@ -66,6 +73,102 @@ const int doorPin = D1, sirenPin = D0, h24Pin = D2, activeAlarmPin = D8;
 // Define flags
 bool alarmActive = false, inAlarm = false, doorOpened = false, tamperOpened = false;
 
+// Server certificate
+static const char serverCert[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+)EOF";
+
+// Server private key
+static const char serverKey[] PROGMEM = R"EOF(
+)EOF";
+
+bool connectToWifi()
+{
+  byte timeout = 50;
+
+  Serial.print("\n\nConnecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  for (int i = 0; i < timeout; i++)
+  {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      Serial.println("\nConnected to WiFi");
+      Serial.print("Server can be accessed at https://");
+      Serial.print(WiFi.localIP());
+      if (MDNS.begin(dname))
+      {
+        Serial.print(" or at https://");
+        Serial.print(dname);
+      }
+      return true;
+    }
+    delay(5000);
+    Serial.print(".");
+  }
+
+  Serial.println("\nFailed to connect to WiFi");
+  Serial.println("Check network status and access data");
+  Serial.println("Push RST to try again");
+  return false;
+}
+
+void showWebpage()
+{
+  String content = "";
+  if (server.hasArg("key"))
+  {
+    if (String(API_KEY).equals(server.arg("key")))
+    {
+      String op = server.arg("operation");
+      if (op == "on")
+      {
+        setAlarm(true);
+        content += "{\"result\":";
+        if (!alarmActive)
+        {
+          content += "false,\"msg\":\"Error. Check the tamper line.\"}";
+        }
+        else
+        {
+          content += "true,\"msg\":\"Alarm activated\"}";
+        }
+      }
+      else if (op == "off")
+      {
+        setAlarm(false);
+        content += "{\"result\":true,\"msg\":\"Alarm deactivated\"}";
+      }
+      else if (op == "status")
+      {
+        content += "{\"door\":";
+        content += digitalRead(doorPin) == HIGH ? "\"open\"" : "\"closed\"";
+        content += ", \"h24\":";
+        content += digitalRead(h24Pin) == HIGH ? "\"open\"" : "\"closed\"";
+        content += ", \"active\":";
+        content += alarmActive ? "true" : "false";
+        content += ", \"alarm\":";
+        content += inAlarm ? "true" : "false";
+        content += "}";
+      }
+
+      server.send(200, "application/json", content);
+    }
+  }
+  else
+  {
+    server.send(500);
+  }
+}
+
+void secureRedirect()
+{
+  serverHTTP.sendHeader("Location", String("https://allarme.local"), true);
+  serverHTTP.send(301, "text/plain", "");
+}
+
 void setup()
 {
   pinMode(doorPin, INPUT_PULLUP);
@@ -88,15 +191,22 @@ void setup()
     };
   }
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
+  while (!connectToWifi())
   {
-    delay(500);
+    delay(60000);
+    ESP.restart();
   }
   Serial.println("Connected.");
   Serial.print("IP address: ");
   Serial.print(WiFi.localIP());
-  Serial.println(" Alarm ready.");
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  serverHTTP.on("/", secureRedirect);
+  serverHTTP.begin();
+  server.getServer().setRSACert(new BearSSL::X509List(serverCert), new BearSSL::PrivateKey(serverKey));
+  server.on("/", showWebpage);
+  server.onNotFound(showWebpage);
   server.begin();
+  Serial.println("\nAlarm ready.");
   SPI.begin();        // Initiate  SPI bus
   mfrc522.PCD_Init(); // Initiate MFRC522
   tamperPreviousTime = millis();
@@ -104,89 +214,12 @@ void setup()
 
 void loop()
 {
-  WiFiClient client = server.available(); // Listen for incoming clients
   rfidCardScanner();
   alarmCheck();
   sirenCheck();
-  if (client)
-  {                          // If a new client connects,
-    String currentLine = ""; // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime)
-    { // loop while the client's connected
-      currentTime = millis();
-      if (client.available())
-      {                         // if there's bytes to read from the client,
-        char c = client.read(); // read a byte, then
-        header += c;
-        if (c == '\n')
-        { // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0)
-          {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Access-Control-Allow-Origin: *");
-            client.println("Access-Control-Max-Age: 2520");
-            client.println("Access-Control-Allow-Methods: GET, PUT, DELETE, XMODIFY");
-            client.println("Content-type:application/json");
-            client.println("Connection: close");
-            client.println();
-
-            // Verify if are present commands in HTTP header
-            if (header.indexOf("GET /1/on") >= 0)
-            {
-              setAlarm(true);
-              client.print("{\"result\":");
-              if (!alarmActive)
-              {
-                client.print("false,\"msg\":\"Error. Check the tamper line.\"}");
-              }
-              else
-              {
-                client.print("true,\"msg\":\"Alarm activated\"}");
-              }
-            }
-            else if (header.indexOf("GET /1/off") >= 0)
-            {
-              setAlarm(false);
-              client.print("{\"result\":true,\"msg\":\"Alarm deactivated\"}");
-            }
-            else if (header.indexOf("GET /status") >= 0)
-            {
-              client.print("{\"door\":");
-              client.print(digitalRead(doorPin) == HIGH ? "\"open\"" : "\"closed\"");
-              client.print(", \"h24\":");
-              client.print(digitalRead(h24Pin) == HIGH ? "\"open\"" : "\"closed\"");
-              client.print(", \"active\":");
-              client.print(alarmActive ? "true" : "false");
-              client.print(", \"alarm\":");
-              client.print(inAlarm ? "true" : "false");
-              client.print("}");
-            }
-            // The HTTP response ends with another blank line
-            // Break out of the while loop
-            break;
-          }
-          else
-          { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        }
-        else if (c != '\r')
-        {                   // if you got anything else but a carriage return character,
-          currentLine += c; // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-  }
+  serverHTTP.handleClient();
+  server.handleClient();
+  MDNS.update();
 }
 
 void rfidCardScanner()
